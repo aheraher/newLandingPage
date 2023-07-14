3880794f332f5b214fab55021ad29ed00b28b081
Dipak Aher <97633930+aheraher@users.noreply.github.com>
2023-07-14 12:44:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+gsap.from("#img1 img", {
+    opacity:0,
+    y:30,
+    duration:1,
+    delay:0.5,
+
+
+})
+
+
+gsap.from("#img2 img", {
+    opacity:0,
+    x:30,
+    duration:1,
+    delay:0.5,
+
+
+})
+gsap.from("#img3 img", {
+    opacity:0,
+    y:-30,
+    duration:1,
+    delay:0.5,
+
+
+})
+
+gsap.from("#middle h1", {
+    opacity:0,
+     duration:1,
+    delay:0.5,
+
+
+})
